828d3828f3c3bcaabb50080b9dbde558fb78b28e
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-09-08 23:23:06 +0200

    solve N+1

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,6 +22,7 @@ class RecipesController < ApplicationController
     if @recipe.save
       redirect_to recipes_path, notice: 'Recipe created successfully.'
     else
+      flash[:alert] = 'Recipe creation failed. Please check the form for errors.'
       render :new
     end
   end
@@ -36,16 +37,19 @@ class RecipesController < ApplicationController
     @recipes = Recipe.public_recipes.includes(:user)
   end
 
-  def make_public
+  def make_public_or_private(make_public)
     @recipe = current_user.recipes.find(params[:id])
-    @recipe.update(public: true)
-    redirect_to @recipe, notice: 'Recipe is now public.'
+    @recipe.update(public: make_public)
+    message = make_public ? 'Recipe is now public.' : 'Recipe is now private.'
+    redirect_to @recipe, notice: message
+  end
+
+  def make_public
+    make_public_or_private(true)
   end
 
   def make_private
-    @recipe = current_user.recipes.find(params[:id])
-    @recipe.update(public: false)
-    redirect_to @recipe, notice: 'Recipe is now private.'
+    make_public_or_private(false)
   end
 
   def shopping_list
@@ -58,7 +62,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :public, :description)
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
   end
 
   def find_missing_foods
