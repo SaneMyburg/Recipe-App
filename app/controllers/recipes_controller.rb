5126b698f74772aa1f8b54465284e8b846badee0
SaneMyburg <sane.myburg@gmail.com>
2023-09-08 10:37:15 +0200

    Create actions for shopping list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -32,6 +32,10 @@ class RecipesController < ApplicationController
     redirect_to recipes_path, notice: 'Recipe deleted successfully.'
   end
 
+  def public_recipes
+    @recipes = Recipe.public_recipes.includes(:user)
+  end
+
   def make_public
     @recipe = current_user.recipes.find(params[:id])
     @recipe.update(public: true)
@@ -44,9 +48,35 @@ class RecipesController < ApplicationController
     redirect_to @recipe, notice: 'Recipe is now private.'
   end
 
+  def shopping_list
+    @recipes = current_user.recipes.includes(:recipe_foods)
+    @missing_foods = find_missing_foods
+    @total_items = @missing_foods.pluck(:quantity).sum
+    @total_price = calculate_total_price(@missing_foods)
+  end
+
   private
 
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :public, :description)
+  end
+
+  def find_missing_foods
+    missing_foods = []
+    @recipes.each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        missing_quantity = recipe_food.quantity
+        missing_foods << recipe_food if missing_quantity&.positive?
+      end
+    end
+    missing_foods
+  end
+
+  def calculate_total_price(missing_foods)
+    total_price = 0
+    missing_foods.each do |recipe_food|
+      total_price += recipe_food.quantity * recipe_food.food.price
+    end
+    total_price
   end
 end
